3678b01cf724c5e398da040b37c1cd1fe0f805f7
gautam-cpp <gautamprajapat2509@gmail.com>
2025-05-15 18:17:16 +0530

    React frontend and add tailwindCss

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="bg-gray-900 text-white h-screen flex items-center justify-center font-medium text-2xl">
+      Tailwind is working Niggas!!
+    </div>
+  )
+}
+
+export default App
