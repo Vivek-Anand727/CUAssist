a4245ea8dd352ef59163e8ac8d2f47c04ddb3618
Sumedh <Sumedhvats2004@gmail.com>
2025-05-15 21:13:13 +0530

    added basic file structure

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,2 +1,9 @@
 // Use prisma client like this
 import { prisma } from "./lib/prisma";
+import express from "express";
+import homeRouter from "./routes/homeRoute";
+const app = express();
+
+
+app.use("/api/v1/", homeRouter);
+app.listen(3000);
---
+++ Backend/src/routes/homeRoute.ts
@@ -0,0 +1,8 @@
+import express  from "express"
+import { homePageController } from "../controllers/homePage";
+
+const homeRouter= express.Router();
+
+
+homeRouter.get("/", homePageController)
+export default homeRouter;
