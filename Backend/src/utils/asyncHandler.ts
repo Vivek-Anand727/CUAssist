f1ef48294440baa8ca932e03269c42d9f5b59b1c
gautam-cpp <gautamprajapat2509@gmail.com>
2025-05-30 23:25:07 +0530

    put middleware to right folder

--- Backend/src/utils/asyncHandler.ts
+++ Backend/src/utils/asyncHandler.ts
@@ -1,5 +1,6 @@
-import { Request, Response, NextFunction } from "express";
+// this middleware/utility function is used to handle async(ts) errors  in express routes
 
+import { Request, Response, NextFunction } from "express";
 
 export function asyncHandler(fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) {
   return (req: Request, res: Response, next: NextFunction) => {
